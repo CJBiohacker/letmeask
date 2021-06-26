70a0e24bcbe76c9ac97aee775190fa58f69971c7
CJBiohacker <cj.moyses@gmail.com>
2021-06-26 15:03:32 -0300

    'Revisao de codigos e arquivos'

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './services/firebase'; // Importação dos serviços de Autenticação e Acesso do Firebase
+import './services/firebase'; // Importação dos serviços de Autenticação e Acesso do Firebase.
 
-import './style/global.scss'  // Importação de Estilização CSS/SASS
-import './style/auth.scss' // Importação de Estilização CSS/SASS
+import './style/global.scss'  // Importação de Estilização CSS/SASS.
+import './style/auth.scss' // Importação de Estilização CSS/SASS.
 
 ReactDOM.render(
   <React.StrictMode>
